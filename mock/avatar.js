25fed689977e3d8809b989e86b0cedfb52bf0cbb
ykx <yingkx@thel.co>
2018-01-19 17:13:55 +0800

    feat: 使用BizCharts作为图表控件

--- mock/avatar.js
+++ mock/avatar.js
@@ -1,3 +1,8 @@
+/**
+ * 头像列表
+ * 同时用来练习下七牛图片裁剪的规则
+ */
+
 export default [
   'http://pro.thel.co/gift/56d9caecc63fbc4759b8.jpg',
   'http://pro.thel.co/gift/dab866cb86fd804da8e9.jpg',
--- src/routes/Dashboard/index.js
+++ src/routes/Dashboard/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Button } from 'antd';
+import { Chart, Geom, Axis, Tooltip, Legend, Coord } from 'bizcharts';
 
 function Dashboard({ dispatch }) {
+    // 数据源
+  const data = [
+    { genre: 'Sports', sold: 275, income: 2300 },
+    { genre: 'Strategy', sold: 115, income: 667 },
+    { genre: 'Action', sold: 120, income: 982 },
+    { genre: 'Shooter', sold: 350, income: 5271 },
+    { genre: 'Other', sold: 150, income: 3710 }
+  ];
+
+  // 定义度量
+  const cols = {
+    sold: { alias: '销售量' },
+    genre: { alias: '游戏种类' }
+  };
+
   function showImageViewer() {
     dispatch({
       type: 'imageViewer/open',
@@ -26,6 +42,15 @@ function Dashboard({ dispatch }) {
     <div>
       <Button onClick={showImageViewer} style={{ marginRight: 10 }}>图片预览</Button>
       <Button onClick={showDetailPanel}>详情面板</Button>
+      <div style={{ marginTop: 40 }}>
+        <Chart width={600} height={400} data={data} scale={cols}>
+          <Axis name="genre" />
+          <Axis name="sold" />
+          <Legend position="top" dy={-20} />
+          <Tooltip />
+          <Geom type="interval" position="genre*sold" color="genre" />
+        </Chart>
+      </div>
     </div>
   );
 }
