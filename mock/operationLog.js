d440c7332c2bd0d726209733ec0e943f6cd7a419
ykx <yingkx@thel.co>
2018-01-26 16:33:42 +0800

    refactor: 去除头像的Mock数据

--- mock/operationLog.js
+++ mock/operationLog.js
@@ -1,11 +1,10 @@
 import mockjs from 'mockjs';
-import avatar from './avatar';
 
 export default {
   'GET /api/operationLog/list': (req, res) => {
     const { list } = mockjs.mock({
       'list|100': [{
-        'avatar|1': avatar,
+        'avatar': '',
         'operator': '@cname',
         'createAt': '@datetime',
         'operationType|1': ['禁用账号', '解禁账号'],
